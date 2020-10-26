12a963e24612db0347f1f4b1db93d86067f504f3
“AlokMishra051298 <Mishra051298alok@gmail.com>
2020-10-26 17:31:27 +0530

    Added next question and add option fuctionality

--- src/components/AddQuiz.jsx
+++ src/components/AddQuiz.jsx
@@ -1,26 +1,30 @@
-import React, {useRef} from 'react'
+import React, { useRef } from 'react'
+import ReactDOM from 'react-dom'
+import Option from './option'
 import { useEffect } from 'react';
 import { useState } from 'react';
 
 function AddQuiz() {
     const optionRef = useRef();
-    let optionNum = 3
-    const [question, setQuestion] = useState();
-    const [optionList, setOptionList] = useState();
-
-    // useEffect(() => {
-    //     let op
-    // });
+    let optionNum = 2
 
     // add option
     let addOption = () => {
-        let optionDiv = document.createElement("div");
+        // option Checkbox
+        let optionCheckBox = document.createElement('input');
+        optionCheckBox.setAttribute('type', 'radio');
+        optionCheckBox.id = `op${optionNum}`;
+        optionCheckBox.name = "correctOption"
+        // option input
         let option = document.createElement("input");
         const optionClasses = ["option", `op${optionNum}`];
         option.placeholder = "Edit Question..."
         option.id = optionClasses[1];
         option.className = "inputOption"
+        // option div
+        let optionDiv = document.createElement("div");
         optionDiv.classList.add(...optionClasses);
+        optionDiv.appendChild(optionCheckBox);
         optionDiv.appendChild(option);
         optionRef.current.appendChild(optionDiv);
         optionNum++;
@@ -31,28 +35,51 @@ function AddQuiz() {
         
     }
 
-    // next question
-    let nextQuestion = () => {
-        let optionList = []
+    // clear input
+    let clearInput = () => {
+        for (let i of document.getElementsByTagName("input")) {
+            i.value="";
+        }
+    }
 
+    // next question
+    let nextQuestion = (e) => {
+        let optionList = []
+        let correctOption = null;
         for (let i of document.getElementsByClassName("inputOption")) {
-            // optionList.push(i.value);
             if (i.value.trim() !== "") {
-                optionList.push(i.value.trim())
+                optionList.push(i.value.trim());
             }
         }
-        if (document.getElementsByClassName('inputQuestion')[0].value.trim() !== "" && optionList.length>1) {
-            console.log(optionList);
+        for (let i of document.getElementsByName('correctOption')) {
+            if (i.checked) {
+                let id = i.id;
+                correctOption = parseInt(id.slice(2,));
+            }
+        }
+        if (document.getElementsByClassName('inputQuestion')[0].value.trim() !== "" && optionList.length > 1 && correctOption !== null) {
+            let data = { "question": document.getElementsByClassName('inputQuestion')[0].value.trim(), "options": optionList, "answer": correctOption };
+            let questionList = JSON.parse(localStorage.getItem("questionList")) || [];
+            // if (localStorage.getItem("questionList")) {
+            //     questionList.push(JSON.parse(localStorage.getItem("questionList")));
+            // }
+            questionList.push(data);
+            localStorage.setItem("questionList", JSON.stringify(questionList));
+            console.log(JSON.parse(localStorage.getItem("questionList")));
+            clearInput();
         }
         else if (document.getElementsByClassName('inputQuestion')[0].value.trim() === "") {
             console.log("Question string cannt be null");
         }
-        else {
+        else if (optionList.length < 2) {
             console.log("Minimum Options are needed");
-            console.log(optionList);
-            console.log("still some error");
+        }
+        else {
+            console.log("please provide the correct option");
         }
     }
+
+
     return (
         <center>
             <h1>Quizzer</h1>
@@ -60,10 +87,12 @@ function AddQuiz() {
                 <input className="inputQuestion" placeholder="Type Question..." />
                 <div className="options" ref={optionRef}>
                     <div className="option op0">
-                        <input id="op1" className="inputOption" placeholder="Edit Question..." />
+                        <input type="radio" id="op0" name="correctOption"/>
+                        <input className="inputOption" placeholder="Edit Question..." />
                     </div>
                     <div className="option op1">
-                        <input id="op2" className="inputOption" placeholder="Edit Question..." />
+                        <input type="radio" id="op1" name="correctOption"/>
+                        <input className="inputOption" placeholder="Edit Question..."/>
                     </div>
                 </div>
                 <button onClick={addOption}>Add Options</button>
